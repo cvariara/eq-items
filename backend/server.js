7d89f9ee3d1d7abe9d93ee5338e05727c3588ce4
Christopher <cvariara@gmail.com>
2024-04-22 16:19:01 -0400

    Updated for production

--- backend/server.js
+++ backend/server.js
@@ -2,76 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env file
 const cors = require('cors');
+const http = require('http');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4000;
 
 const itemsRoutes = require('./routes/items');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// MongoDB connection URI
-const mongoUri = process.env.MONGODB_URI;
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
 
 // Define routes
 app.use('/api/items', itemsRoutes);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
-// const express = require('express');
-// const { MongoClient } = require('mongodb');
-// require('dotenv').config(); // Load environment variables from .env file
-// const cors = require('cors');
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// const itemsRoutes = require('./routes/items');
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // MongoDB connection URI
-// const uri = process.env.MONGODB_URI;
-
-// // Create a new MongoDB client
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// // Connect to MongoDB
-// async function connectToMongoDB() {
-//   try {
-//     await client.connect();
-//     console.log('Connected to MongoDB');
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//   }
-// }
-
-// // Define routes
-// app.use('/api/items', itemsRoutes);
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-//   connectToMongoDB(); // Connect to MongoDB when server starts
-// });
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    server.listen(PORT, () => {
+      console.log('Connected to DB');
+      console.log(`Listening on port ${PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
